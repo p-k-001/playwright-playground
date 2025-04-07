7a5f93a1aa1b58a8094408b415a7304e99e0ecee
pk <pk@pk.com>
2025-04-07 11:54:08 +0200

    first commit

---
+++ tests/my-test.spec.ts
@@ -0,0 +1,10 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("my-test", () => {
+  test("Check the title of the todo app", async ({ page }) => {
+    await page.goto("https://demo.playwright.dev/todomvc");
+    // await expect(page).toHaveTitle("React • TodoMVC");
+
+    await expect(page).toHaveTitle(/.*TodoMVC$/);
+  });
+});
